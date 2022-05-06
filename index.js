70b72a427555a8c237c582b26d3527dd41f7cbd1
Andriy-FrfR <andriy.lukashewwich@gmail.com>
2022-05-06 17:01:18 +0300

    .

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build/static'));
+  app.use(express.static('./client/build/static'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
